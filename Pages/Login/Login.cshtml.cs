7e126ee867e4e7197ffee92cef15801157d91a1d
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-09-18 01:36:26 -0300

    feat: updated boostrap and added materialize css

---
+++ Pages/Login/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Top_Seguros_Brasil_Web.Pages.Login
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
