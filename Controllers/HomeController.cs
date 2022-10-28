57b2d5c62ff52bcc847aebd5c97e5ed8c95608ce
RaissaMarques <raissamarquesb@gmail.com>
2022-10-27 22:39:53 -0300

    nutella2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,20 +13,26 @@ namespace TopSegurosBrasilWeb.Controllers
             _logger = logger;
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+
+      
+
 }
