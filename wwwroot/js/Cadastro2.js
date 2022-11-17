fe24ffb8e98b1a7bd5d777e417deb66fc0cac65f
Guilherme de Souza Santiago <guisantiagostart@gmail.com>
2022-11-17 17:11:11 -0300

    Cadastro2 js

---
+++ wwwroot/js/Cadastro2.js
@@ -0,0 +1,9 @@
+﻿async function Pagina2() {
+    var cpf = document.getElementById("cpf").value;
+    var cnh = document.getElementById("cnh").value;
+
+    localStorage.setItem("cpf", cpf);
+    localStorage.setItem("cnh", cnh);
+
+    updateLocalStorage();
+}
