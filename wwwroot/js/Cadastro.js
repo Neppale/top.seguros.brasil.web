20ad77c2472bae02d4632f28bfb3949a6aa55b86
Guilherme de Souza Santiago <guisantiagostart@gmail.com>
2022-11-17 10:33:56 -0300

    salvando localStorage

---
+++ wwwroot/js/Cadastro.js
@@ -0,0 +1,9 @@
+﻿async function cadastraCliente() {
+    var nomeCompleto = document.getElementById("nomeCompleto").value;
+    var dataNascimento = document.getElementById("dataNascimento").value;
+
+    localStorage.setItem("nome", nomeCompleto);
+    localStorage.setItem("dataNascimento", dataNascimento);
+
+    updateLocalStorage();
+}
--- wwwroot/js/Validando.js
+++ wwwroot/js/Validando.js
@@ -22,4 +22,3 @@
         alert('Dados incorretos');
     }
 }  
-//ss
