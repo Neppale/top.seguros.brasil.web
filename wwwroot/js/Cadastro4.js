49fc91f546fff95c7f0688673841e8018ec2625b
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-12-01 23:46:54 -0300

    fix: alterada forma de fazer requisiçoes cadastro iv

--- wwwroot/js/Cadastro4.js
+++ wwwroot/js/Cadastro4.js
@@ -1,25 +1,36 @@
 ﻿async function getMarcas() {
-  var retornoMarca = await fetch(
+  const retornoMarca = await fetch(
     "https://tsb-api-policy-engine.herokuapp.com/fipe/marcas/"
   ).then((retornoMarca) => {
     return retornoMarca.json();
   });
-  //para cada marca, adicionar um option no select com o valor do codigo e o nome da marca.
   for (var i = 0; i < retornoMarca.length; i++) {
     $("#inputGroupSelect01").append(
       `<option value='${retornoMarca[i].codigo}'>${retornoMarca[i].nome}</option>`
     );
   }
 }
+
+addEventListener("click", (e) => {
+  if (e.target.id === "inputGroupSelect02") {
+    getModelo();
+  } else if (e.target.id === "inputGroupSelect03") {
+    getAno();
+  }
+});
+
 async function getModelo() {
-  //pegar o value do inputGroupSelect01 option selecionado e passar como parametro para a url da api de modelos.
-  var codigoMarca = $("#inputGroupSelect01").val();
-  var retornoModelo = await fetch(
+  const codigoMarca = $("#inputGroupSelect01").val();
+  if (codigoMarca == "Marcas") return;
+
+  const retornoModeloJSON = await fetch(
     `https://tsb-api-policy-engine.herokuapp.com/fipe/marcas/${codigoMarca}/modelos`
-  ).then((retornoModelo) => {
-    return retornoModelo.json();
-  });
-  //para cada modelo, adicionar um option no select com o valor do codigo e o nome do modelo.
+  );
+  const retornoModelo = await retornoModeloJSON.json();
+
+  const inputGroupSelect02 = document.getElementById("inputGroupSelect02");
+  inputGroupSelect02.innerHTML = "Modelo";
+
   for (var i = 0; i < retornoModelo.length; i++) {
     $("#inputGroupSelect02").append(
       `<option value='${retornoModelo[i].codigo}'>${retornoModelo[i].nome}</option>`
@@ -27,15 +38,18 @@ async function getModelo() {
   }
 }
 async function getAno() {
-  //pegar o value do inputGroupSelect02 option selecionado e passar como parametro para a url da api de anos.
-  var codigoMarca = $("#inputGroupSelect01").val();
-  var codigoModelo = $("#inputGroupSelect02").val();
-  var retornoAno = await fetch(
+  const codigoMarca = $("#inputGroupSelect01").val();
+  const codigoModelo = $("#inputGroupSelect02").val();
+
+  if (codigoMarca == "Marcas" || codigoModelo == "Modelo") return;
+  const retornoAnoJSON = await fetch(
     `https://tsb-api-policy-engine.herokuapp.com/fipe/marcas/${codigoMarca}/modelos/${codigoModelo}/anos`
-  ).then((retornoAno) => {
-    return retornoAno.json();
-  });
-  //para cada ano, adicionar um option no select com o valor do codigo e o nome do ano.
+  );
+  const retornoAno = await retornoAnoJSON.json();
+
+  const inputGroupSelect03 = document.getElementById("inputGroupSelect03");
+  inputGroupSelect03.innerHTML = "Ano";
+
   for (var i = 0; i < retornoAno.length; i++) {
     $("#inputGroupSelect03").append(
       `<option value='${retornoAno[i].codigo}'>${retornoAno[i].nome}</option>`
@@ -43,11 +57,18 @@ async function getAno() {
   }
 }
 async function pagina4() {
-  var nomeMarca = $("#inputGroupSelect01 option:selected").text();
-  var nomeModelo = $("#inputGroupSelect02 option:selected").text();
-  var anoVeiculo = $("#inputGroupSelect03 option:selected").text();
+  const nomeMarca = $("#inputGroupSelect01 option:selected").text();
+  const nomeModelo = $("#inputGroupSelect02 option:selected").text();
+  const anoVeiculo = $("#inputGroupSelect03 option:selected").text();
+
+  if (nomeMarca == "Marcas" || nomeModelo == "Modelo" || anoVeiculo == "Ano") {
+    alert("É necessário preencher todos os campos antes de continuar.");
+    return false;
+  }
 
   localStorage.setItem("marca", nomeMarca);
   localStorage.setItem("modelo", nomeModelo);
   localStorage.setItem("ano", anoVeiculo);
+
+  window.location.href = "CadastroV";
 }
